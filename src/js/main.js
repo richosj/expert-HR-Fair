d7fa8ba2659f56994cc6fde315cad4d8cddbb581
richosj <44463951+richosj@users.noreply.github.com>
2025-07-02 01:22:17 +0900

    dd

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-console.log('Hello Vite!');
+import '@/scss/main.scss';
+
+const body = document.querySelector('body');
+const openApplyBtn = document.querySelector('.open-apply__btn');
+const openApply = document.querySelector('.open-apply');
+
+// 참가신청 버튼 클릭 시 참가신청 모달 창 열기
+openApplyBtn.addEventListener('click', () => {
+    body.classList.toggle('apply-open');
+});
+
--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,7 +1,3 @@
-
-import { buttonClick, toggleTab } from '../common/ui.js'
-
 document.addEventListener('DOMContentLoaded', () => {
-  toggleTab('.main-tab')
-  buttonClick('.main-button')
+
 })
