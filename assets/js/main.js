a41692d309c06762e027904b8e9ee5e0201ff46f
richosj <okm760596@gmail.com>
2025-07-04 15:12:06 +0900

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,6 +36,23 @@
   }
 })();
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+if (window.innerWidth > 768) {
+  ScrollTrigger.normalizeScroll(true);
+  ScrollTrigger.config({ autoRefreshEvents: "visibilitychange,DOMContentLoaded,load" });
+  ScrollSmoother.create({
+    wrapper: "#smoother-wrapper",
+    content: "#smoother-content",
+    smooth: 1.1,
+    speed: 1,
+    normalizeScroll: true,
+    ignoreMobileResize: true,
+    smoothTouch: 0.1,
+    effects: true,
+    preventDefault: true,
+    normalizeScroll: { allowNestedScroll: true }
+    // 중첩 스크롤 허용
+  });
+}
 const body = document.querySelector("body");
 const openApplyBtn = document.querySelector(".open-apply__btn");
 document.querySelector(".open-apply");
@@ -46,34 +63,8 @@ openApplyBtn.addEventListener("click", () => {
 applyClose.addEventListener("click", () => {
   body.classList.remove("apply-open");
 });
-const applyBtn = document.querySelector(".open-apply");
-const footer = document.querySelector(".footer");
-ScrollTrigger.create({
-  trigger: footer,
-  start: () => `top bottom`,
-  // footer top이 viewport bottom에 닿을 때
-  end: () => `bottom bottom`,
-  // footer bottom이 viewport bottom에 닿을 때
-  scrub: false,
-  onEnter: () => {
-    const footerTop = footer.offsetTop;
-    const btnHeight = applyBtn.offsetHeight;
-    gsap.set(applyBtn, {
-      position: "absolute",
-      top: `${footerTop - btnHeight}px`,
-      right: 0,
-      bottom: "auto"
-    });
-  },
-  onLeaveBack: () => {
-    gsap.set(applyBtn, {
-      position: "fixed",
-      bottom: 0,
-      right: 0,
-      top: "auto"
-    });
-  }
-});
+document.querySelector(".open-apply");
+document.querySelector(".footer");
 const modal = document.getElementById("customModal");
 const modalClose = document.getElementById("customModalClose");
 const modalBackdrop = modal == null ? void 0 : modal.querySelector(".custom-modal__backdrop");
